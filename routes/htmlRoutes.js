62714447f078c2ca048c8a69d74129f3883a9b4a
StephenTHendrix <stephenthendrix@gmail.com>
2019-10-31 10:23:20 -0500

    fixed linting issues

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,12 +12,14 @@ module.exports = function(app) {
   });
 
   app.get("/signup", function(req, res) {
-  res.render("index_create"); 
+    res.render("index_create");
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
