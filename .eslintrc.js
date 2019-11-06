8b29c227859980abc553e81c1fde763d613ce338
ALayendecker <alcommits@gmail.com>
2019-11-05 22:52:36 -0600

    prepping merge

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: { es6: true },
-  parserOptions: {
-    ecmaVersion: 2017
-  },
-  rules: {
-    "react/jsx-no-target-blank": 0
-  }
-};
--- server/models/Task.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Task = sequelize.define("Task", {
-    text: DataTypes.STRING
-  });
-  Task.associate = function({ Board }) {
-    Task.belongsTo(Board);
-  };
-  return Task;
-};
